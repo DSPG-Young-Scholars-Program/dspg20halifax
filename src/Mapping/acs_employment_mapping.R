7e64f6de60185f199f7f429e73e6a95acdf8bd21
Ellen Graham <eg5se@virginia.edu>
2020-06-25 17:27:09 -0400

    updated map for new data location

--- src/Mapping/acs_employment_mapping.R
+++ src/Mapping/acs_employment_mapping.R
@@ -2,37 +2,38 @@ library(leaflet)
 library(purrr)
 library(dplyr)
 library(glue)
+library(sf)
 
-source(here::here("src", "Data_Ingestion", "acs_employment_injestion.R"))
+acs_unemployment_county_sp <- st_read(here::here("data", "original", "acs_unemployment_county.geojson"))
 
 # -------------------------------------------------------------------------------------------------
 
 # check ranges for three variables of interest
-# range(unemployment_county$estimate_unemployment_rate_population_16_years_and_over_estimate)
+# range(acs_unemployment_county_sp$estimate_unemployment_rate_population_16_years_and_over_estimate)
 # 0.6 23.3
-# range(unemployment_county$estimate_unemployment_rate_population_16_years_and_over_race_and_hispanic_or_latino_origin_white_alone_estimate)
+# range(acs_unemployment_county_sp$estimate_unemployment_rate_population_16_years_and_over_race_and_hispanic_or_latino_origin_white_alone_estimate)
 # 0.6 14.1
-# range(unemployment_county$estimate_unemployment_rate_population_16_years_and_over_race_and_hispanic_or_latino_origin_black_or_african_american_alone_estimate, na.rm = TRUE)
+# range(acs_unemployment_county_sp$estimate_unemployment_rate_population_16_years_and_over_race_and_hispanic_or_latino_origin_black_or_african_american_alone_estimate, na.rm = TRUE)
 # 0.0, 31.7
 
 # check distributions
-# hist(unemployment_county$estimate_unemployment_rate_population_16_years_and_over_estimate)
-# hist(unemployment_county$estimate_unemployment_rate_population_16_years_and_over_race_and_hispanic_or_latino_origin_white_alone_estimate)
-# hist(unemployment_county$estimate_unemployment_rate_population_16_years_and_over_race_and_hispanic_or_latino_origin_black_or_african_american_alone_estimate)
+# hist(acs_unemployment_county_sp$estimate_unemployment_rate_population_16_years_and_over_estimate)
+# hist(acs_unemployment_county_sp$estimate_unemployment_rate_population_16_years_and_over_race_and_hispanic_or_latino_origin_white_alone_estimate)
+# hist(acs_unemployment_county_sp$estimate_unemployment_rate_population_16_years_and_over_race_and_hispanic_or_latino_origin_black_or_african_american_alone_estimate)
 # breaks seem reasonable
 
 # function to check if moe very high, which I'm calling > 50% of value. Returns NA for elements that are
 
 check_unstable <- function(variable_name) {
-  ifelse((unemployment_county_sp[[glue("{variable_name}_estimate")]]) < 2 * unemployment_county_sp[[glue("{variable_name}_moe")]],
+  ifelse((acs_unemployment_county_sp[[glue("{variable_name}_estimate")]]) < 2 * acs_unemployment_county_sp[[glue("{variable_name}_moe")]],
          NA,
-         unemployment_county_sp[[glue("{variable_name}_estimate")]])
+         acs_unemployment_county_sp[[glue("{variable_name}_estimate")]])
 
 }
 
 # quick map of unemployment by white and black. Note that unemployments above 25% are assumed unstable
 unemployment_scale <- colorBin("BuPu", c(0,100), c(0, 2.5, 5, 7.5, 10, 30))
-m <- leaflet(unemployment_county_sp) %>%
+m <- leaflet(acs_unemployment_county_sp) %>%
   addTiles() %>%  # Add default OpenStreetMap map tiles
   addPolygons(color = "#444444", weight = 0.5, smoothFactor = 0.5,
               opacity = 1.0, fillOpacity = 0.8,
