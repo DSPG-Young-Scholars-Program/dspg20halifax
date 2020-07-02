e08a780691d9320502e5d48d7468027dcb3da9b9
Ellen Graham <eg5se@virginia.edu>
2020-07-02 14:56:58 -0400

    added gini index to inequality measures

--- src/Mapping/acs_income_inequalty_mapping.R
+++ src/Mapping/acs_income_inequalty_mapping.R
@@ -19,3 +19,22 @@ create_map(top_bottom_difference,
            scale_breaks = c(90000, 150000, 200000, 250000, 300000, 420000),
            unstable_threshold = 2)
 
+
+gini_index <- tidycensus::get_acs(geography = "county",
+                      year = 2018,
+                      variables = c(gini_index = "B19083_001"),
+                      state = "VA") %>%
+  tidyr::pivot_wider(names_from = variable,
+                     values_from = c(estimate, moe),
+                     names_glue = "{variable}_{.value}")
+
+tigris::counties(state = "VA", cb = TRUE, resolution = "20m", class = "sf") %>%
+  st_transform(crs = 4326) %>%
+  left_join(gini_index, by = "GEOID") %>%
+  create_map(variables= c("gini_index"),
+             group_names = NULL,
+             legend_name = "Gini Index",
+             label_name = "Gini Index",
+             scale_domain = c(0.3, 0.6),
+             scale_breaks = c(0.3,0.4, 0.42, 0.45, 0.5, 0.6),
+             unstable_threshold = 2)
