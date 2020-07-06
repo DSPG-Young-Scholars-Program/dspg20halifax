b0a5ad285f49d89cd4614adcca409b766f3da1eb
Finn Roberts <robe2037@umn.edu>
2020-07-06 09:21:36 -0400

    Updated FBI API data pulling script

--- src/Data_Ingestion/ucr_data_ingestion.R
+++ src/Data_Ingestion/ucr_data_ingestion.R
@@ -4,39 +4,49 @@ library(jsonlite)
 library(dplyr)
 library(stringr)
 
-## Offenses to view (all available for certain datasets)
-offenses <- c("aggravated-assault", "burglary", "larceny", "motor-vehicle-theft", "homicide", "rape", "robbery", "arson", "violent-crime")
+#
+#
+# Script to pull data from FBI's NIBRS database using their API --------------------------------------------------------------------------
+#
+#
 
-# ---- Halifax offenses ---- #
+## You will need an API key to pull data. The script assumes you have stored your key in the R environment.
+## API Keys can be obtained here: https://api.data.gov/signup/
+## More documentation about data source here: https://crime-data-explorer.fr.cloud.gov/api
 
-## Codes for each police entity in Halifax
-halifax_sheriff_ori <- "VA0410000"
-halifax_police_ori <- "VA0410100"
-south_boston_ori <- "VA1240000"
-sp_halifax_ori <- "VA041SP00"
-sp_south_boston_ori <- "VA124SP00"
 
-ori_codes <- c(halifax_sheriff_ori, halifax_police_ori, south_boston_ori, sp_halifax_ori, sp_south_boston_ori)
 
-## Generic function to pull data from the API
+# ---- Helpful Functions ---- #
+
+## Basic function to pull data from the API given a specific path
 get_nibrs <- function(api_path) {
   
   req <- GET(paste0(api_path, "?API_KEY=", Sys.getenv("UCR_API_KEY")))
-  req_text <- fromJSON(content(req, as = "text"))
   
-  if ("data" %in% names(req_text)) {
-    return(req_text$data)
+  status <- http_status(req)
+  
+  if (status$category == "Success") {
+    
+    req_text <- fromJSON(content(req, as = "text"))
+    
+    if ("data" %in% names(req_text)) {
+      return(req_text$data)
+    } else {
+      return(req_text$results)
+    }
+    
   } else {
-    return(req_text$results)
+    stop(status$message)
   }
   
 }
 
-## Function that uses the get_nibrs function to pull demographic data specifically
-get_nibrs_demo <- function(offenses, ## What crimes to pull
-                           person, ## "offender" or "victim" data
-                           ori_codes, 
-                           demo_var) {
+## Function that uses the get_nibrs function to pull demographic data at the agency level
+get_nibrs_agency <- function(offenses, ## What crimes to pull
+                             person, ## "offender" or "victim" data
+                             ori_codes, ## Vector of codes for specific agencies
+                             demo_var ## Single demographic variable of interest
+                             ) {
   
   i <- 1
   data_list <- list()
@@ -45,9 +55,8 @@ get_nibrs_demo <- function(offenses, ## What crimes to pull
     for (ori in ori_codes) {
       
       ## Assemble path for this combination of variables
-      path <- paste("https://api.usa.gov/crime/fbi/sapi/api/nibrs", offense, person, "agencies", state, ori, demo_var, sep = "/")
+      path <- paste("https://api.usa.gov/crime/fbi/sapi/api/nibrs", offense, person, "agencies", ori, demo_var, sep = "/")
       
-      print(path)
       ## Pull data
       data <- get_nibrs(path)
       
@@ -73,33 +82,82 @@ get_nibrs_demo <- function(offenses, ## What crimes to pull
   
 }
 
-halifax_age_data <- get_nibrs_demo(offenses = offenses, person = "offender", ori_codes = ori_codes, demo_var = "age")
-halifax_race_data <- get_nibrs_demo(offenses = offenses, person = "offender", ori_codes = ori_codes, demo_var = "race")
-halifax_sex_data <- get_nibrs_demo(offenses = offenses, person = "offender", ori_codes = ori_codes, demo_var = "sex")
+## Function that uses the get_nibrs function to pull demographic data at the state level
+get_nibrs_state <- function(offenses, ## What crimes to pull
+                            person, ## "offender" or "victim" data
+                            state, ## single state abbreviation
+                            demo_var ## Demographic variable of interest
+                            ) {
+  
+  i <- 1
+  data_list <- list()
+  
+  for (offense in offenses) {
+    
+    ## Assemble path for this combination of variables
+    path <- paste("https://api.usa.gov/crime/fbi/sapi/api/nibrs", offense, person, "states", state, demo_var, sep = "/")
+    
+    ## Pull data
+    data <- get_nibrs(path)
+    
+    ## Make sure there is data for this combination
+    if (length(data) != 0) {
+      data$offense <- offense
+      data$demo_var <- demo_var
+      data$state <- state
+      
+      data_list[[i]] <- data
+      
+      i <- i + 1
+    }
+    
+  }
+  
+  ## Combine data into single source
+  combined <- do.call(bind_rows, data_list)
+  combined <- combined %>% select(state, data_year, offense, demo_var, everything())
+  
+  return(combined)
+  
+}
 
-## Still not totally clear on the differences between these two but for simplicity I get the counts from the "offender" API path as well..
-halifax_offense_counts <- get_nibrs_demo(offenses = offenses, person = "offender", geog_level = "agencies", ori_codes = ori_codes, demo_var = "count")
-#halifax_offense_counts <- get_nibrs_demo(offenses = offenses, person = "offense", geog_level = "agencies", ori_codes = ori_codes, demo_var = "count")
+
+
+# ---- Halifax offenses ---- #
+
+## Offenses to view (all that are available for certain datasets)
+offenses <- c("aggravated-assault", "burglary", "larceny", "motor-vehicle-theft", "homicide", "rape", "robbery", "arson", "violent-crime")
+
+## Codes for each police entity in Halifax
+halifax_sheriff_ori <- "VA0410000"
+halifax_police_ori <- "VA0410100"
+south_boston_ori <- "VA1240000"
+sp_halifax_ori <- "VA041SP00"
+sp_south_boston_ori <- "VA124SP00"
+
+ori_codes <- c(halifax_sheriff_ori, halifax_police_ori, south_boston_ori, sp_halifax_ori, sp_south_boston_ori)
+
+## Pull data for each demographic variable of interest
+halifax_age_data <- get_nibrs_agency(offenses = offenses, person = "offender", ori_codes = ori_codes, demo_var = "age")
+halifax_race_data <- get_nibrs_agency(offenses = offenses, person = "offender", ori_codes = ori_codes, demo_var = "race")
+halifax_sex_data <- get_nibrs_agency(offenses = offenses, person = "offender", ori_codes = ori_codes, demo_var = "sex")
+
+## Still not totally clear on the differences between these two but for comparability I get the counts from the "offender" API path as well..
+halifax_offense_counts <- get_nibrs_agency(offenses = offenses, person = "offender", ori_codes = ori_codes, demo_var = "count")
+#halifax_offense_counts <- get_nibrs_agency(offenses = offenses, person = "offense", geog_level = "agencies", ori_codes = ori_codes, demo_var = "count")
+
+
 
 # ---- State level offenses ---- #
 
-## Combine data from all offense types into single dataset
-i <- 1
-data_list <- list()
+## Counts by type of crime and demographics at state level
+va_sex_data <- get_nibrs_state(offenses = offenses, person = "offender", state = "VA", demo_var = "sex")
+va_age_data <- get_nibrs_state(offenses = offenses, person = "offender", state = "VA", demo_var = "age")
+va_race_data <- get_nibrs_state(offenses = offenses, person = "offender", state = "VA", demo_var = "race")
 
-for (offense in offenses) {
-  
-  ucr_get <- GET(paste0("https://api.usa.gov/crime/fbi/sapi/api/nibrs/", offense, "/offender/states/VA/count?API_KEY=", Sys.getenv("UCR_API_KEY")))
-  ucr_list <- fromJSON(content(ucr_get, as = "text"))
-  ucr_data <- ucr_list$data
-  
-  ucr_data$offense <- offense
-  
-  data_list[[i]] <- ucr_data
-  
-  i <- i + 1
-}
 
-va_offense_data <- do.call(bind_rows, data_list)
 
+# ---- Write data for easier access ---- #
+
+# readr::write_csv(va_race_data, here::here("data", "original", "Crime", "va_race_data.csv"))
 
