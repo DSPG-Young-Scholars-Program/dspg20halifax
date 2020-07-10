334a5e745b8e018a24b31fa4c1a3d7c99f41b6c2
Finn Roberts <robe2037@umn.edu>
2020-07-10 12:02:09 -0400

    Added comments and moved function to map_template.R

--- src/Data_Ingestion/housing_data_import.R
+++ src/Data_Ingestion/housing_data_import.R
@@ -11,7 +11,9 @@ library(ggplot2)
 
 census_api_key(Sys.getenv("CENSUS_API_KEY"))
 
-source(here("src", "Data_Ingestion", "acs_multi_year_import.R"))
+#source(here("src", "Mapping", "acs_se_viz_experiment.R"))
+source(here("src", "Mapping", "map_template.R"))
+#source(here("src", "Data_Ingestion", "acs_multi_year_import.R"))
 
 ## Variable names for ACS
 vars_2018 <- load_variables(2018, dataset = "acs5", cache = TRUE)
@@ -25,19 +27,15 @@ halifax_tracts <- tracts(state = "VA", county = "Halifax", class = "sf", cb = TR
   st_transform(crs = 4326)
 
 ## ACS table IDs
-tables <- c("B07013", ## Geographic mobility
-            "S2507", ## Financial characteristics - no mortgage
-            "S2504", ## Physical characteristics
-            "S2502", ## Demographic characteristics
-            "DP04") ## General housing statistics
+# tables <- c("B07013", ## Geographic mobility
+#             "S2507", ## Financial characteristics - no mortgage
+#             "S2504", ## Physical characteristics
+#             "S2502", ## Demographic characteristics
+#             "DP04") ## General housing statistics
 
 # ----- Geographic mobility ---- #
 
-#geog_mobility <- get_acs_multi_years(table = "S0701", var_names = subject_vars_2018, survey = "acs1", years = 2010)
-#geog_mobility <- get_acs_multi_years(table = "S0701", var_names = subject_vars_2018, survey = "acs5", years = 2018)
-
-#geojson_write(geog_mobility, geometry = "polygon", file = here("data", "original", "Housing", "acs5_2018_geog_mobility.geojson"))
-
+## County level geographic mobility variables
 geog_mobility <- get_acs(geography = "county",
                          year = 2018,
                          table = "S0701",
@@ -49,6 +47,7 @@ geog_mobility <- get_acs(geography = "county",
               values_from = c(estimate, moe),
               names_glue = "{label}_{.value}")
 
+## Tract level geographic mobility variables
 geog_mobility_tracts <- get_acs(geography = "tract",
                                 year = 2018,
                                 table = "S0701",
@@ -61,39 +60,38 @@ geog_mobility_tracts <- get_acs(geography = "tract",
               values_from = c(estimate, moe),
               names_glue = "{label}_{.value}")
 
+## Merge ACS on spatial data
 geog_mobility_counties <- left_join(va_counties, geog_mobility, by = c("GEOID"))
 geog_mobility_tracts <- left_join(halifax_tracts, geog_mobility_tracts, by = c("GEOID"))
 
-
-source(here("src", "Mapping", "acs_se_viz_experiment.R"))
-
+## Plot county map
 geog_mobility_counties %>% 
   create_map(variable = "estimate_moved_within_same_county_population_1_year_and_over", 
              group_names = "Intra-County Mobility",
              legend_name = "Intra-County Mobility",
              label_name = "Intra_County Mobility",
-             scale_domain = range(geog_mobility_counties$`estimate_moved;_within_same_county_population_1_year_and_over_estimate`),
+             scale_domain = range(geog_mobility_counties$`estimate_moved_within_same_county_population_1_year_and_over_estimate`),
+             scale_breaks = c(0, 2, 4, 6, 8, 10, 12),
+             unstable_threshold = 0)
 
-             
-             unstable_threshold = 2)
-
+## Plot tract map
 geog_mobility_tracts %>% 
   create_map(variable = "estimate_moved_within_same_county_population_1_year_and_over", 
              group_names = "Intra-County Mobility",
              legend_name = "Intra-County Mobility",
              label_name = "Intra_County Mobility",
              scale_domain = range(geog_mobility_counties$`estimate_moved;_within_same_county_population_1_year_and_over_estimate`),
-             bins = 5,
+             scale_breaks = c(0, 2, 4, 6, 8, 10, 12),
              unstable_threshold = 0)
 
-pal <- colorBin("BuPu", c(0, max(geog_mobility_tracts$`estimate_moved;_within_same_county_population_1_year_and_over_estimate`) + max(geog_mobility_tracts$`estimate_moved;_within_same_county_population_1_year_and_over_moe`)), bins = 5, na.color = "#fafafa")
+## Sample from independent normals at tract level and plot across layers to show uncertainty in estimates
+pal <- colorBin("BuPu", c(0, 12), bins = 5, na.color = "gray")
 
 map_samples(data = geog_mobility_tracts, 
             var = "estimate_moved_within_same_county_population_1_year_and_over_estimate", 
             se_var = "estimate_moved_within_same_county_population_1_year_and_over_moe", 
             x = 5, 
             palette = pal)
-
 
 # ----- Rent percentage of income ---- #
 
