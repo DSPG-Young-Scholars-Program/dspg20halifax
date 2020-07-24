1da6b6fd11d52c34e4b085623529dc034078d852
Finn Roberts <robe2037@umn.edu>
2020-07-24 11:19:43 -0400

    cleaned county strings in HUD data, refined single mother viz

--- src/EDA/Housing/pub_housing_eda.R
+++ src/EDA/Housing/pub_housing_eda.R
@@ -5,29 +5,74 @@ library(stringr)
 library(ggplot2)
 library(gghighlight)
 library(tidyr)
+library(leaflet)
+library(sf)
 
-va_pub_housing_summary <- data.table::fread(here("data", "original", "Housing", "county_pub_housing_2013_2019.csv")) %>% 
-  filter(state == "VA", year > 2012) %>%
+## Spatial data for VA if needed
+# va_counties <- tigris::counties(state = "VA", year = 2018, cb = TRUE, resolution = "20m") %>% 
+#   st_as_sf() %>% 
+#   st_transform(crs = 4326)
+
+## Read in all public housing data and clean county strings
+pub_housing_summary <- vroom::vroom(here("data", "original", "Housing", "county_pub_housing_2013_2019.csv")) %>%
   replace("NA", NA) %>%
-  as.data.frame()
+  mutate(across(everything(), function(col) ifelse(col == -1 | col == -4 | col == -5, NA, col))) %>% ## -1 = NA, -4 = Suppressed, -5 = % reporting too low
+  mutate(name = str_extract(str_replace_all(str_to_lower(name), "[0-9]{3} ", ""), "^([^,])+"), ## County names have numbers for some years, include state name after a comma for some years. Clean these
+         name = str_to_title(trimws(ifelse(str_detect(name, "city county"), str_replace_all(name, "county", ""), name)))) ## Clean duplicated counties that are labeled as both city and county
 
-hud_programs <- va_pub_housing_summary %>% 
-  filter(program_label == "Summary of All HUD Programs")
-
-va_pub_housing_summary %>%
-  filter(!is.na(total_units), name == "Halifax County", pct_occupied > 0) %>%
-  mutate(total_units = as.numeric(total_units)) %>%
-  group_by(program_label, name, year) %>%
-  mutate(tot_occupied = total_units * pct_occupied / 100, tot_unoccupied = (1 - pct_occupied / 100) * total_units) %>%
-  filter(year > 2013) %>%
-  pivot_longer(cols = c("tot_unoccupied", "tot_occupied"), names_to = "occupation_status") %>%
-  mutate(occupation_status = factor(occupation_status, levels = c("tot_unoccupied", "tot_occupied"))) %>%
+## Pct of households headed by a single parent (most are females in Halifax - can show)
+pub_housing_summary %>%
+  filter(year == 2019, program_label == "Summary of All HUD Programs") %>%
+  mutate(isVA = ifelse(state == "VA", TRUE, FALSE),
+         isHalifax = ifelse(state == "VA" & name == "Halifax County", TRUE, FALSE)) %>%
   ggplot() +
-  geom_bar(aes(x = reorder(program_label, -value), y = value, fill = occupation_status), color = "#E7E6DC", stat = "identity", alpha = 0.9) +
-  scale_fill_manual(values = c("#d8d3bc", "#6b6385")) +
-  facet_wrap(~year) +
+  geom_point(aes(x = pct_black_nonhsp, y = pct_1adult, color = interaction(isVA, isHalifax), size = pct_lt24_head, alpha = isVA), stroke = 1.25) +
+  coord_flip() +
+  scale_size_continuous(range = c(0, 20)) +
+  #facet_wrap(~year) +
+  scale_color_manual(values = c("#e4e0cd", "#6b6385", "#FC4444")) +
+  scale_alpha_manual(values = c(0.3, 0.8)) +
   theme_minimal()
 
+## Want to confirm wether there is pattern to missingness:
+# pub_housing_summary %>%
+#   filter(year == 2019, program_label == "Summary of All HUD Programs") %>%
+#   summarize(sum(is.na(pct_lt24_head)))
 
 
 
+## Really want to compare to pct_black of county - get from ACS
+p1 <- pub_housing_summary %>%
+  filter(year == 2019) %>%
+  mutate(pct_female_head_child = ifelse(pct_female_head_child == -1, NA, pct_female_head_child)) %>%
+  mutate(isVA = ifelse(states == "VA Virginia", TRUE, FALSE),
+         isHalifax = ifelse(states == "VA Virginia" & name == "Halifax County", TRUE, FALSE)) %>%
+  ggplot() +
+  geom_point(aes(x = pct_black_nonhsp, y = pct_female_head, color = interaction(isVA, isHalifax), size = pct_lt24_head, alpha = isVA), stroke = 1.25) +
+  coord_flip() +
+  scale_size_continuous(range = c(0, 20)) +
+  #facet_wrap(~year) +
+  scale_color_manual(values = c("#e4e0cd", "#6b6385", "#FC4444")) +
+  scale_alpha_manual(values = c(0.3, 0.8)) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+## Really want to compare to pct_black of county - get from ACS
+## Percent of households headed by female that have children
+p2 <- pub_housing_summary %>%
+  filter(year == 2019) %>%
+  mutate(pct_female_head_child = ifelse(pct_female_head_child == -1, NA, pct_female_head_child)) %>%
+  mutate(isVA = ifelse(states == "VA Virginia", TRUE, FALSE),
+         isHalifax = ifelse(states == "VA Virginia" & name == "Halifax County", TRUE, FALSE)) %>%
+  ggplot() +
+  geom_point(aes(x = pct_black_nonhsp, y = pct_female_head_child, color = interaction(isVA, isHalifax), size = pct_lt24_head, alpha = isVA), stroke = 1.25) +
+  coord_flip() +
+  scale_size_continuous(range = c(0, 20)) +
+  #facet_wrap(~year) +
+  scale_color_manual(values = c("#e4e0cd", "#6b6385", "#FC4444")) +
+  scale_alpha_manual(values = c(0.3, 0.8)) +
+  theme_minimal()
+
+# gridExtra::grid.arrange(p1, p2, ncol = 2)
+
+# plotly::ggplotly(p2)
