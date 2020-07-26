2b776978ad27499405db25cd4d1cb3111b2d9e57
Riya Berry <rb4ba@virginia.edu>
2020-07-26 19:32:33 -0400

    finished and comments added

--- src/Data_Ingestion/SA_opiod_use_halifax.R
+++ src/Data_Ingestion/SA_opiod_use_halifax.R
@@ -1,12 +1,17 @@
 library(ggplot2)
 
+#read CSV
 datasource <- readr::read_csv(here::here("git", "TestDSPG", "Halifaxx", "data",
                                          "original", "Substance_Abuse",
                                          "CDC Opioid Prescription Data - Halifax (1).csv"))
-head(datasource)
+total <- datasource$`US Total`
+
+#plot the data
 ggplot(data = datasource, aes(x= Year)) +
-  geom_line(aes(y = Halifax, colour = "red")) + geom_line(aes(y = "US Total", colour = "green"))+
+  #plot of Halifax and VA state over time
+  geom_line(aes(y = Halifax, colour = "red")) + geom_line(aes(y = total, colour = "green")) +
   labs(y = 'Opioid Prescription Rates per 100 People') +
-  scale_color_discrete(name = "Location", labels = c("Halifax, VA", "US"))
-  geom_point(aes(y = Halifax)) + geom_point(aes(y = "US Total"))
+  #relabel the scale appropriately
+  scale_color_discrete(name = "Location", labels = c("Halifax, VA", "US")) +
+  geom_point(aes(y = Halifax)) + geom_point(aes(y = total))
 
